5538cf98d94d2215428a71ec1e19695553e2a7fb
nandaluridoc <69188982+nandaluridoc@users.noreply.github.com>
2022-01-07 16:23:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ zone = "us-east1-b"
 resource "google_compute_instance" "vm_instance" {
   name         = "test"
   machine_type = "f1-micro"
-
+tags         = ["vm_instance"]
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
